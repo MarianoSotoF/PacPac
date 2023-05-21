acbe1e53c6b833e3b5f026b1b3b0a3161d306438
poki-musi <poki.musi@gmail.com>
2023-05-21 12:19:31 +0200

    finish orange monster + add better labyrinth

--- Assets/Scripts/Enemies/Orange/Orange_EnemyMovement.cs
+++ Assets/Scripts/Enemies/Orange/Orange_EnemyMovement.cs
@@ -4,29 +4,65 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Orange_EnemyMovement : MonoBehaviour {
-    private bool playerInSight;
     private Vector3 origin;
     private Transform player;
     private NavMeshAgent pathfinder;
 
-    public bool PlayerInSight {
-        get { return playerInSight; }
-        set { playerInSight = value; }
+    private float timeInsideTrigger;
+    private float timeOutsideTrigger;
+
+    private bool playerInRange;
+    public bool PlayerInRange {
+        get { return playerInRange; }
+        set { 
+            if(!value) {
+                timeOutsideTrigger = timeInsideTrigger;
+                pathfinder.ResetPath();
+            }
+            playerInRange = value; 
+        }
     }
 
     void Start() {
         origin = transform.position;
-        playerInSight = false;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        playerInRange = false;
+        timeOutsideTrigger = 0.0f;
+        timeInsideTrigger = 3.0f;
     }
 
     void FixedUpdate()
     {
-        if(playerInSight)
+        if(playerInRange) { // move towards played if inside area
             pathfinder.SetDestination(player.position);
-        else {
+            if(DetectPlayerInSight()) {
+                pathfinder.speed = 3.8f;
+            } else {
+                pathfinder.speed = 1.5f;
+            }
+        } else if(timeOutsideTrigger > 0.0f) { // wait for a bit once player is out of area
+            timeOutsideTrigger -= Time.deltaTime;
+        } else { // go back if player is outside area for long enough
             pathfinder.SetDestination(origin);
+            pathfinder.speed = 4f;
+        }
+    }
+
+    private bool DetectPlayerInSight() {
+        Vector3 playerPos = player.transform.position;
+        Vector3 orangePos = transform.position;
+        Vector3 dirRay = (orangePos - playerPos);
+        dirRay.Normalize();
+
+        // Vector3 interactionRayEndpoint = dirRay * 1000 + playerPos;
+        // Debug.DrawLine(playerPos, interactionRayEndpoint, new Color(0f, 0f, 1f));
+
+        RaycastHit interactionRayhit;
+        if(Physics.Raycast(new Ray(playerPos, dirRay), out interactionRayhit, float.PositiveInfinity)) {
+            return interactionRayhit.transform.gameObject.CompareTag("Orange_Monster");
+        } else {
+            return false;
         }
     }
 }
